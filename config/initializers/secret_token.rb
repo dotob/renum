e089341621b91cd7edcb78cfe8172dedcaccc1c7
dotob <sk@dotob.de>
2012-03-15 22:26:05 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Renum::Application.config.secret_token = 'e631ab775edaaa14789975bb5f76d03bb6a37d399312dc5c0c9947feeb2aef1f16a58d11c8d4dd9bc1b48ab709567ba05fa7e8c4a5e9a3a26533ecdf6e0253ee'
