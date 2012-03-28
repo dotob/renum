e9a512a78af37718cd39f03a02b1a08f911a8aa7
dotob <sk@dotob.de>
2012-03-28 22:27:54 +0200

    update precompiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,7 @@ Renum::Application.configure do
 
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
+  config.assets.precompile << ["*.coffee"]
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
